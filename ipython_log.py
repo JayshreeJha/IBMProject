7a730b20804d6571f1210b49ac1987525b99b3b1
JayshreeJha <103238721+JayshreeJha@users.noreply.github.com>
2023-09-10 21:04:12 +0530

    Delete ipython_log.py

--- ipython_log.py
+++
@@ -1,4 +0,0 @@
-# IPython log file
-
-get_ipython().run_line_magic('logstop', '')
-runfile('C:/Users/Jayshree/OneDrive/Desktop/IBM-Project/Flask/Flask/app.py', wdir='C:/Users/Jayshree/OneDrive/Desktop/IBM-Project/Flask/Flask')
